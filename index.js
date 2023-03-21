41efc57723f78ce3951e8c9f8f1fadac071b5e61
Eduardo <eduardo.didax@hotmail.com>
2023-03-20 21:31:42 -0300

    Nomeando o projeto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.send('Inicio de um site para ver filmes e series')
+    res.send('Inicio do MovieWave um site para ver filmes e series')
   })
   
   app.listen(3000);
